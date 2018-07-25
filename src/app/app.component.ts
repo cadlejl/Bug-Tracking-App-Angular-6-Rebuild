57829f1c0ca9fe92a8d847e0faeccff27a4e8796
Jaime Cadle <jaime@coficiency.com>
2018-07-25 08:10:31 -0500

    add logo; set app.component template and styles; install bootstrap and add to angular.json; begin to add routing.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`.container {margin-top: 5rem;}`]
 })
 export class AppComponent {
   title = 'app';
